939761421851fd49d47b896e85b4bb40744a5aa1
bingog <1870394711@qq.com>
2023-04-13 13:31:26 +0800

    cross_platform_file

--- out/create_project/initProject.js
+++ out/create_project/initProject.js
@@ -6,8 +6,10 @@ const node_fs_1 = require("node:fs");
 const path = require("path");
 function createInitProject(filepath, context) {
     vscode.window.showInformationMessage("createInitProject");
-    filepath = filepath.replace(/\\/g, '\/');
-    filepath = "file:///" + filepath;
+    //filepath = filepath.replace(/\\/g, '\/');
+    let urlPrefix = "file:///";
+    filepath = path.join(urlPrefix, filepath);
+    //filepath = "file:///" + filepath;
     vscode.window.showInformationMessage("path is :" + path + "filepath is : " + filepath);
     let fileName = "PRBS.xml";
     let pyName = "fiber.py";
@@ -15,16 +17,19 @@ function createInitProject(filepath, context) {
     let copyPyPath = context.asAbsolutePath(path.join('py_tomls', pyName));
     let copyTomlPath = context.asAbsolutePath(path.join('py_tomls', tomlName));
     (0, node_fs_1.mkdirSync)(new URL(filepath));
-    (0, node_fs_1.mkdirSync)(new URL(filepath + '\/src'));
-    (0, node_fs_1.mkdirSync)(new URL(filepath + '\/src\/test'));
-    (0, node_fs_1.mkdirSync)(new URL(filepath + '\/.vscode'));
-    //copyFileSync(copyFromPath, filepath);
+    //mkdirSync(new URL(filepath + '\/src'));
+    (0, node_fs_1.mkdirSync)(new URL(path.join(filepath, "src")));
+    //mkdirSync(new URL(filepath + '\/src\/test'));
+    (0, node_fs_1.mkdirSync)(new URL(path.join(filepath, "src", "test")));
+    //mkdirSync(new URL(filepath + '\/.
+    //copyFileSync(copyFromPath, filepath);vscode'));
+    (0, node_fs_1.mkdirSync)(new URL(path.join(filepath, ".vscode")));
     //get local resource URI used in webview 
     //let data = readFileSync(copyFromPath);
     let optiDevPyName = "opti_dev.py";
     let optiDevTomlName = "opti_dev.toml";
-    (0, node_fs_1.copyFileSync)(copyPyPath, new URL(filepath + "\/src\/" + optiDevPyName));
-    (0, node_fs_1.copyFileSync)(copyTomlPath, new URL(filepath + "\/" + optiDevTomlName));
+    (0, node_fs_1.copyFileSync)(copyPyPath, new URL(path.join(filepath, "src", optiDevPyName)));
+    (0, node_fs_1.copyFileSync)(copyTomlPath, new URL(path.join(filepath, optiDevTomlName)));
 }
 exports.createInitProject = createInitProject;
 //# sourceMappingURL=initProject.js.map
--- src/create_project/initProject.ts
+++ src/create_project/initProject.ts
@@ -1,10 +1,13 @@
 import * as vscode from "vscode";
 import { readFileSync, copyFileSync, writeFileSync, mkdirSync} from "node:fs";
 import * as path from 'path';
+import { url } from "node:inspector";
 export function createInitProject(filepath : string, context: vscode.ExtensionContext) {
    vscode.window.showInformationMessage("createInitProject");
-   filepath = filepath.replace(/\\/g, '\/');
-   filepath = "file:///" + filepath;
+   //filepath = filepath.replace(/\\/g, '\/');
+   let urlPrefix = "file:///";
+   filepath = path.join(urlPrefix, filepath);
+   //filepath = "file:///" + filepath;
    vscode.window.showInformationMessage("path is :" + path + "filepath is : " + filepath);
    let fileName = "PRBS.xml";
    let pyName = "fiber.py";
@@ -12,14 +15,17 @@ export function createInitProject(filepath : string, context: vscode.ExtensionCo
    let copyPyPath = context.asAbsolutePath(path.join('py_tomls', pyName));
    let copyTomlPath = context.asAbsolutePath(path.join('py_tomls', tomlName));
    mkdirSync(new URL(filepath));
-   mkdirSync(new URL(filepath + '\/src'));
-   mkdirSync(new URL(filepath + '\/src\/test'));
-   mkdirSync(new URL(filepath + '\/.vscode'));
-   //copyFileSync(copyFromPath, filepath);
+   //mkdirSync(new URL(filepath + '\/src'));
+   mkdirSync(new URL(path.join(filepath, "src")));
+   //mkdirSync(new URL(filepath + '\/src\/test'));
+   mkdirSync(new URL(path.join(filepath, "src", "test")));
+   //mkdirSync(new URL(filepath + '\/.
+   //copyFileSync(copyFromPath, filepath);vscode'));
+   mkdirSync(new URL(path.join(filepath, ".vscode")));
    //get local resource URI used in webview 
    //let data = readFileSync(copyFromPath);
    let optiDevPyName = "opti_dev.py";
    let optiDevTomlName = "opti_dev.toml";
-   copyFileSync(copyPyPath, new URL(filepath + "\/src\/" + optiDevPyName));
-   copyFileSync(copyTomlPath, new URL(filepath + "\/" + optiDevTomlName));
+   copyFileSync(copyPyPath, new URL(path.join(filepath, "src", optiDevPyName)));
+   copyFileSync(copyTomlPath, new URL(path.join(filepath, optiDevTomlName)));
 }
